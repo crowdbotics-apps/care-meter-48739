34cdfe6aabdfec8164549f625908a860477f75d3
Crowdbotics <team@crowdbotics.com>
2024-07-12 20:00:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "care-meter-48739",
+  url: "https://care-meter-48739.botics.co",
+  api: "https://care-meter-48739.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
